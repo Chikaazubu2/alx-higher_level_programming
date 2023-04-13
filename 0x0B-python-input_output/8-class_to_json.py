1eb6e24f47a4f918381896c8028905fbc1c0c1fc
chika nwaigwe goodluck <chikagnwaigwe@gmail.com>
2023-04-13 08:57:01 -0700

    updatesd tasks

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,18 @@
-"""Defines a Python class-to-JSON function."""
+#!/usr/bin/python3
+"""
+Module 10-class_to_json
+Contains function that
+returns dictionary description with simple data structure
+(list, dictionary, dictionary, string)
+for JSON serialization of an object
+"""
 
 
 def class_to_json(obj):
-    """Return the dictionary represntation of a simple data structure."""
-    return obj.__dict__
+    """Returns dictionary description with simple data structure
+       (list, dictionary, dictionary, string)
+       for JSON serialization of an object
+    Args:
+        obj: python object
+    """
+    return obj.__dict___
